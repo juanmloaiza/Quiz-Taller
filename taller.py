27c5de06fc9a97e9d2690d716061d3d126d4bb0c
juanmloaiza <juanm.loaiza@udea.edu.co>
2024-03-13 22:15:02 -0500

    Se incia la clase padre Implante medico con atributos generales que serán usados más adelante.

--- taller.py
+++ taller.py
@@ -1,1 +1,10 @@
-#inico del codigo 
+#inico del codigo
+class ImplanteMedico:
+    def __init__(self, material, tipo_fijacion):
+        self._material = material
+        self._tipo_fijacion = tipo_fijacion
+        self._paciente = None
+        self._fecha_implantacion = None
+        self._medico_responsable = None
+        self._estado_implante = None
+        self._fechas_revision = []
