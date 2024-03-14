bb5304cfc3575bd93e0500fd4ea073487dc44937
juanmloaiza <juanm.loaiza@udea.edu.co>
2024-03-13 22:18:50 -0500

    Se crean los metodos para acceder a los atributos privados de las clasa implante medico.

--- taller.py
+++ taller.py
@@ -8,3 +8,33 @@ class ImplanteMedico:
         self._medico_responsable = None
         self._estado_implante = None
         self._fechas_revision = []
+    
+    def get_material(self):
+        return self._material
+
+    def set_material(self, material):
+        self._material = material
+
+    def get_tipo_fijacion(self):
+        return self._tipo_fijacion
+
+    def set_tipo_fijacion(self, tipo_fijacion):
+        self._tipo_fijacion = tipo_fijacion
+
+    def asignar_paciente(self, paciente, fecha_implantacion, medico_responsable, estado_implante):
+        self._paciente = paciente
+        self._fecha_implantacion = fecha_implantacion
+        self._medico_responsable = medico_responsable
+        self._estado_implante = estado_implante
+
+    def obtener_seguimiento(self):
+        return {
+            'Paciente': self._paciente,
+            'Fecha de Implantación': self._fecha_implantacion,
+            'Médico Responsable': self._medico_responsable,
+            'Estado del Implante': self._estado_implante,
+            'Fechas de Revisión': self._fechas_revision if self._fechas_revision else "No hay fechas de revisión"
+        }
+
+    def agregar_revision(self, fecha_revision):
+        self._fechas_revision.append(fecha_revision)
